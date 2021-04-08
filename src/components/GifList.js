5f5757eefd65e7201c9b44b6cd6dbd1d393a7a76
mateusz0612 <dennyfacio@gmail.com>
2021-04-08 22:32:45 +0200

    working with GifList componenet also added loading cicle

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
+import { BiSad } from "react-icons/bi";
+import axios from "axios";
+import "../css/GifList.css";
 
 const GifList = () => {
-  return <h2>Here will be list of trending gifs</h2>;
+  const initialState = {
+    items: [],
+    isLoading: true,
+    isError: false,
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_ITEMS":
+        return { ...state, items: action.payload };
+      case "SET_LOADING":
+        return { ...state, isLoading: false };
+      case "SET_ERROR":
+        return { ...state, isError: true };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getData = async () => {
+    const response = await axios({
+      method: "get",
+      url: `https://api.giphy.com/v1/gifs/trending?api_key=tmf7Jc3lOhYC42tUvzsPix4bqRDx1FOz`,
+    }).catch((err) => {
+      if (err.response) {
+        dispatch({ type: "SET_ERROR" });
+      }
+    });
+    if (response !== undefined) {
+      const data = response.data.data;
+      dispatch({ type: "SET_LOADING" });
+      dispatch({ type: "SET_ITEMS", payload: data });
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (state.isLoading) {
+    return (
+      <section className="wrapper">
+        <div className="loader"></div>
+      </section>
+    );
+  }
+
+  return (
+    <main>
+      <article className="error">
+        {state.isError ? (
+          <React.Fragment>
+            <h1>Couldn't load data from server. Please try again! </h1>
+            <BiSad className="icon" />
+          </React.Fragment>
+        ) : (
+          ""
+        )}
+      </article>
+    </main>
+  );
 };
 
 export default GifList;
