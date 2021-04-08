51a979b63717f7eeb23bf440bda5cbb18d0280ff
mateusz0612 <dennyfacio@gmail.com>
2021-04-08 17:34:38 +0200

    added component,styles folder started working on navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Trending</li>
+        <li>Search</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
