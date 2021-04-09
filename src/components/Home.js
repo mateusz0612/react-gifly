627d63476f6c70524dee02572142a33ccf8776da
mateusz0612 <dennyfacio@gmail.com>
2021-04-09 18:02:46 +0200

    added homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../css/home.css";
 
 const Home = () => {
   return (
     <main>
-      <h2>Home page</h2>
+      <section className="about">
+        <h1>Welcome at Gifly!</h1>
+        <h2>What is gifly?</h2>
+        <p className="description">
+          Gifly is a React application for searching gifs using Giphy API. Go to{" "}
+          <span>Trending</span> to see trending gifs. You can also go to{" "}
+          <span>Search</span> to find gifs that match query you gave. Click on
+          gif <span>title</span> to get more info about him.
+        </p>
+        <h3>Happy hacking!</h3>
+      </section>
     </main>
   );
 };
